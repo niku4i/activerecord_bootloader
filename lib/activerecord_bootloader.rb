67790ddd0004566cc1026c8ad036fb033db73fee
Nobuhiro Nikushi <deneb.ge@gmail.com>
2013-07-17 20:01:03 +0900

    a method for initializing

--- lib/activerecord_bootloader.rb
+++ lib/activerecord_bootloader.rb
@@ -1,5 +1,34 @@
 require "activerecord_bootloader/version"
+require 'active_record'
 
 module ActiverecordBootloader
-  # Your code goes here...
+  module_function
+  
+  # @options Hash
+  # e.g. connect to MySQL
+  #
+  # { 
+  #    adapter: 'mysql2',
+  #       host: 'localhost',
+  #   username: 'me',
+  #   password: 'secret',
+  #   database: 'myapp',
+  # }
+  #
+  # @return ActiveRecord::Base
+  def load_database(options)
+    @@database = nil
+    @@options = options
+    database
+  end
+
+  def database
+    if @@database
+      @@database
+    else
+      ActiveRecord::Base.establish_connection(@@options)
+      ActiveRecord::Base
+    end
+  end
+
 end
